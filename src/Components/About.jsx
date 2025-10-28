e758be799e24423126d59bf91126443418db3ec7
Kyle Javier <kyrncjvr@gmail.com>
2025-10-28 10:06:49 +0000

    Update personal information and skills in App and About components

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,22 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an ICT student at University of Sunshine Coast - Adelaide Campus. I enjoy coding and designing user-friendly interfaces that makes technology accessible to all.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "JavaScript",
+  "C# (.NET Framework)",
+  "MySQL and MongoDB",
+  "Git, Github, and Agile Methodologies",
+  "REST APIs",
+  "PHP",
+  "Swift",
+  "AWS and Azure",
 ];
 
 /**
@@ -45,7 +48,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging the knowledge I gain, I continually look for new and better ways to bring ideas to life.";
 
 const About = () => {
   return (
